2887c82215b6799e17c070c2a8126963ec5ffc7c
chent5 <chen2015tong@gmail.com>
2026-09-08 18:38:30 +0200

    update jvm runtime section

---
+++ jvm-runtime-section/src/main/java/base_compiler/bytecode/MainApp.java
@@ -0,0 +1,8 @@
+package base_compiler.bytecode;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ jvm-runtime-section/src/main/java/runtime_api/JavaProcessId.java
@@ -0,0 +1,12 @@
+package runtime_api;
+
+// ProcessHandle 关于Java进程的处理接口: 获取Process进程相关的信息和状态
+public class JavaProcessId {
+
+    // 获取操作系统分配给Java进程的identification number
+    public static void main(String[] args) throws InterruptedException {
+        // Returns a ProcessHandle for the current process.
+        long pid = ProcessHandle.current().pid();
+        System.out.println(pid);
+    }
+}
