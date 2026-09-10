080bf15f9f5ef5bbe1ed77eb6cdf8f989bf96e27
chent5 <chen2015tong@gmail.com>
2026-09-10 17:12:52 +0200

    update jvm gc and java runtime

--- java-runtime-process/src/main/java/compiler/bytecode/MainApp.java
+++
@@ -1,8 +0,0 @@
-package compiler.bytecode;
-
-public class MainApp {
-
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
-}
---
+++ jvm-runtime-impl/src/main/java/gc/demo/GCCollectObjectsTest.java
@@ -0,0 +1,9 @@
+package gc.demo;
+
+public class GCCollectObjectsTest {
+
+    public static void main(String[] args) {
+        GCCollectObjects collectObjects = new GCCollectObjects();
+        collectObjects.testGcCollectObject();
+    }
+}
