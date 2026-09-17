d64846a1f7971a9fa8ed3d2cb2ebc2d68cf3f0c2
chent5 <chen2015tong@gmail.com>
2026-09-17 17:20:43 +0200

    refresh project

--- java-runtime-api/src/main/java/JavaProcessId.java
+++ java-runtime-api/src/main/java/JavaProcessId.java
@@ -1,14 +1,14 @@
 
-// ProcessHandle: 关于Java进程的处理接口, 获取Process进程相关信息
+// 在代码层面获取到进程ID
 public class JavaProcessId {
 
-    // 在代码层面获取到进程ID
+    // ProcessHandle: 关于Java进程的处理接口, 获取Process进程相关信息
     public static void main(String[] args) {
         long pid = ProcessHandle.current().pid();
         System.out.println(pid);
 
         try {
-            Thread.sleep(1600000);
+            Thread.sleep(16000000);
         } catch (Exception exception) {
             exception.printStackTrace();
         }
