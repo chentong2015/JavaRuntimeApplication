d62926eb6c307242a093933637e486bc19e0ce01
chent5 <chen2015tong@gmail.com>
2026-09-17 14:18:58 +0200

    refresh class file and jvm runtime

--- off-heap-memory/src/main/java/AllocationOutsideMemory.java
+++ off-heap-memory/src/main/java/AllocationOutsideMemory.java
@@ -1,13 +1,9 @@
-package native_memory;
-
 import java.nio.ByteBuffer;
 
 // TODO. Java程序能够引用JVM以外的内存: 实现Zero-Copy零拷贝
 public class AllocationOutsideMemory {
 
-    // ByteBuffer.allocate()
-    // 使用HeapByteBuffer直接在JVM的数据区"堆"内存中分配空间
-    // 添加的数据可以直接通过buffer查看
+    // HeapByteBuffer.allocate() 直接在JVM的数据区"堆"内存中分配空间
     public static void main(String[] args) {
         ByteBuffer buffer = ByteBuffer.allocate(1000);
         for (int j = 0; j < 200; j++) {
