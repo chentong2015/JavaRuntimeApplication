3116e2beb0ad422216f8d69b142d715a1f35ec1f
chent5 <chen2015tong@gmail.com>
2026-09-17 15:51:02 +0200

    add oracle graal vm

--- java-runtime-api/src/main/java/JavaProcessId.java
+++ java-runtime-api/src/main/java/JavaProcessId.java
@@ -1,3 +1,4 @@
+
 // ProcessHandle: 关于Java进程的处理接口, 获取Process进程相关信息
 public class JavaProcessId {
 
@@ -5,5 +6,11 @@ public class JavaProcessId {
     public static void main(String[] args) {
         long pid = ProcessHandle.current().pid();
         System.out.println(pid);
+
+        try {
+            Thread.sleep(1600000);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
